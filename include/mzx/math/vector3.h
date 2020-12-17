18b0e38eb8ab76e3fa7817e8a663ae67b9b8aa7f
mengzhaoxiang <mengzhaoxiang@xd.com>
2020-12-17 18:37:41 +0800

    1

--- include/mzx/math/vector3.h
+++ include/mzx/math/vector3.h
@@ -1,18 +1,28 @@
 #ifndef __MZX_VECTOR3_H__
 #define __MZX_VECTOR3_H__
 
+#include <cmath>
+
 namespace mzx
 {
     template <typename T>
-    class Vector3Util
+    class Vector3MathUtil
     {
     public:
-        static T Epsilon();
-        static T EpsilonNormalSqrt();
-        static T FromInt(int value);
-        static T FromFraction(int numerator, int denominator);
-        static T Clamp(T t, T mint, T maxt);
-        static T Sqrt(T t);
+        using RType = T;
+
+    public:
+        static RType Epsilon();
+        static RType Zero();
+        static RType One();
+        static RType Two();
+        static RType SmoothTimeMin();
+        static RType Dot48();
+        static RType DOT235();
+        static RType Sqrt(const RType &a);
+        static RType Rad2Deg(const RType &a);
+        static RType Deg2Rad(const RType &a);
+        static RType Acos(const RType &a);
     };
 
     template <typename T>
@@ -20,20 +30,24 @@ namespace mzx
     {
     public:
         using RType = T;
+        using MathUtil = Vector3MathUtil<RType>;
 
     private:
-        static const RType R_EPSILON_NORMAL_SQRT = 1e-15f;
-        static const RType R_EPSILON = 1e-5f;
-        static const RType R_ZERO = 0;
-        static const RType R_ONE = 1;
-        static const RType R_TWO = 2;
-        static const RType R_SMOOTH_TIME_MIN = 1e-4f;
-        static const RType R_DOT48 = 0.48f;
-        static const RType R_DOT235 = 0.235f;
+        static const RType R_EPSILON;
+        static const RType R_SQR_EPSILON;
+        static const RType R_ZERO;
+        static const RType R_ONE;
+        static const RType R_TWO;
+        static const RType R_SMOOTH_TIME_MIN;
+        static const RType R_DOT48;
+        static const RType R_DOT235;
 
     public:
-        Vector3() = default;
-        explicit Vector3(RType x, RType y, RType z)
+        Vector3()
+            : x_(R_ZERO), y_(R_ZERO), z_(R_ZERO)
+        {
+        }
+        explicit Vector3(const RType &x, const RType &y, const RType &z)
             : x_(x), y_(y), z_(z)
         {
         }
@@ -43,7 +57,7 @@ namespace mzx
         {
             return x_;
         }
-        void SetX(RType x)
+        void SetX(const RType &x)
         {
             x_ = x;
         }
@@ -51,7 +65,7 @@ namespace mzx
         {
             return y_;
         }
-        void SetY(RType y)
+        void SetY(const RType &y)
         {
             y_ = y;
         }
@@ -59,25 +73,27 @@ namespace mzx
         {
             return z_;
         }
-        void SetZ(RType z)
+        void SetZ(const RType &z)
         {
             z_ = z;
         }
-        void Set(RType x, RType y, RType z)
+        void Set(const RType &x, const RType &y, const RType &z)
         {
             x_ = x;
             y_ = y;
             z_ = z;
+        }
+        void Set(const RType *arr)
+        {
+            x_ = arr[0];
+            y_ = arr[1];
+            z_ = arr[2];
         }
         void Scale(const Vector3 &s)
         {
             x_ *= s.x_;
             y_ *= s.y_;
             z_ *= s.z_;
-        }
-        bool Equals(const Vector3 &other) const
-        {
-            return x_ == other.x_ && y_ == other.y_ && z_ == other.z_;
         }
         void Normalize()
         {
@@ -105,7 +121,7 @@ namespace mzx
         }
         RType Magnitude() const
         {
-            return Vector3Util<RType>::Sqrt(x_ * x_ + y_ * y_ + z_ * z_);
+            return MathUtil::Sqrt(x_ * x_ + y_ * y_ + z_ * z_);
         }
 
     public:
@@ -125,25 +141,25 @@ namespace mzx
         {
             return Vector3(a.x_ - b.x_, a.y_ - b.y_, a.z_ - b.z_);
         }
-        static Vector3 operator*(const Vector3 &a, RType s)
-        {
-            return Vector3(a.x_ * s, a.y_ * s, a.z_ * s);
-        }
-        static Vector3 operator*(RType s, const Vector3 &a)
-        {
-            return Vector3(a.x_ * s, a.y_ * s, a.z_ * s);
-        }
-        static Vector3 operator/(const Vector3 &a, RType d)
-        {
-            return Vector3(a.x_ / d, a.y_ / d, a.z_ / d);
-        }
         static Vector3 operator-(const Vector3 &a)
         {
             return Vector3(-a.x_, -a.y_, -a.z_);
         }
+        static Vector3 operator*(const Vector3 &a, const RType &s)
+        {
+            return Vector3(a.x_ * s, a.y_ * s, a.z_ * s);
+        }
+        static Vector3 operator*(const RType &s, const Vector3 &a)
+        {
+            return Vector3(a.x_ * s, a.y_ * s, a.z_ * s);
+        }
+        static Vector3 operator/(const Vector3 &a, const RType &d)
+        {
+            return Vector3(a.x_ / d, a.y_ / d, a.z_ / d);
+        }
         static bool operator==(const Vector3 &a, const Vector3 &b)
         {
-            return Distance(a, b) < R_EPSILON * R_EPSILON;
+            return SqrDistance(a, b) <= R_SQR_EPSILON;
         }
         static bool operator!=(const Vector3 &a, const Vector3 &b)
         {
@@ -151,60 +167,53 @@ namespace mzx
         }
 
     public:
-        static Vector3 Lerp(const Vector3 &a, const Vector3 &b, RType t)
+        static Vector3 Lerp(const Vector3 &a, const Vector3 &b, const RType &t)
         {
-            t = Vector3Util<RType>::Clamp(t, R_ZERO, R_ONE);
+            auto s = RClamp(t, R_ZERO, R_ONE);
+            return Vector3(a.x_ + (b.x_ - a.x_) * s, a.y_ + (b.y_ - a.y_) * s, a.z_ + (b.z_ - a.z_) * s);
+        }
+        static Vector3 LerpUnclamped(const Vector3 &a, const Vector3 &b, const RType &t)
+        {
             return Vector3(a.x_ + (b.x_ - a.x_) * t, a.y_ + (b.y_ - a.y_) * t, a.z_ + (b.z_ - a.z_) * t);
         }
-        static Vector3 LerpUnclamped(const Vector3 &a, const Vector3 &b, RType t)
-        {
-            return Vector3(a.x_ + (b.x_ - a.x_) * t, a.y_ + (b.y_ - a.y_) * t, a.z_ + (b.z_ - a.z_) * t);
-        }
-        static Vector3 MoveTowards(const Vector3 &current, const Vector3 &target, RType max_distance)
+        static Vector3 MoveTowards(const Vector3 &current, const Vector3 &target, const RType &max_distance)
         {
             auto x = target.x_ - current.x_;
             auto y = target.y_ - current.y_;
             auto z = target.z_ - current.z_;
             auto sqr_dist = x * x + y * y + z * z;
-            if (sqr_dist == R_ZERO || (max_distance >= R_ZERO && sqr_dist <= max_distance * max_distance))
+            if (sqr_dist <= R_SQR_EPSILON || (max_distance >= R_ZERO && sqr_dist <= max_distance * max_distance))
             {
                 return target;
             }
-            auto dist = Vector3Util<RType>::Sqrt(sqr_dist);
+            auto dist = MathUtil::Sqrt(sqr_dist);
             return Vector3(current.x + x * max_distance / dist, current.y + y * max_distance / dist, current.z + z * max_distance / dist);
         }
-        static Vector3 SmoothDamp(Vector3 current, Vector3 target, Vector3 &current_velocity, RType smooth_time, RType max_speed, RType delta_time)
+        static Vector3 SmoothDamp(const Vector3 &current, const Vector3 &target, Vector3 &current_velocity, const RType &smooth_time, const RType &max_speed, const RType &delta_time)
         {
-            if (smooth_time < R_SMOOTH_TIME_MIN)
-            {
-                smooth_time = R_SMOOTH_TIME_MIN;
-            }
-
-            auto omega = R_TWO / smooth_time;
+            auto stime = RMax(smooth_time, R_SMOOTH_TIME_MIN);
+            auto omega = R_TWO / stime;
             auto x = omega * delta_time;
             auto exp = R_ONE / (R_ONE + x + R_DOT48 * x * x + R_DOT235 * x * x * x);
 
             auto change = current - target;
-            auto max_change = max_speed * smooth_time;
+            auto max_change = max_speed * stime;
             auto sqr_mag = change.SqrMagnitude();
             if (sqr_mag > max_change * max_change)
             {
-                change = change * max_change / Vector3Util<RType>::Sqrt(sqr_mag);
+                change = change * max_change / MathUtil::Sqrt(sqr_mag);
             }
-
-            auto original_to = target;
-            target = current - change;
 
             auto temp = (current_velocity + change * omega) * delta_time;
             current_velocity = (current_velocity - temp * omega) * exp;
-            auto output = target + (change + temp) * exp;
+            auto output = (current - change) + (change + temp) * exp;
 
-            auto orig_minus_current = original_to - current;
-            auto out_minus_orig = output - original_to;
+            auto orig_minus_current = target - current;
+            auto out_minus_orig = output - target;
             if (Dot(orig_minus_current, out_minus_orig) > R_ZERO)
             {
-                output = original_to;
-                current_velocity = (output - original_to) / delta_time;
+                output = target;
+                current_velocity = (output - target) / delta_time;
             }
             return output;
         }
@@ -254,11 +263,11 @@ namespace mzx
         }
         static RType Angle(const Vector3 &from, const Vector3 &to)
         {
-            auto denominator = Vector3Util<RType>::Sqrt(from.SqrMagnitude() * to.SqrMagnitude());
-            if (denominator > R_EPSILON * R_EPSILON)
+            auto denominator = MathUtil::Sqrt(from.SqrMagnitude() * to.SqrMagnitude());
+            if (denominator > R_SQR_EPSILON)
             {
-                auto dot = Vector3Util<RType>::Clamp(Dot(from, to) / denominator, -R_ONE, R_ONE);
-                return Vector3Util<RType>::Rad2Deg(Vector3Util<RType>::Acos(dot));
+                auto dot = RClamp(Dot(from, to) / denominator, -R_ONE, R_ONE);
+                return MathUtil::Rad2Deg(MathUtil::Acos(dot));
             }
             return R_ZERO;
         }
@@ -273,16 +282,20 @@ namespace mzx
             }
             return -Angle(from, to);
         }
+        static RType SqrDistance(const Vector3 &a, const Vector3 &b)
+        {
+            return (a - b).SqrMagnitude();
+        }
         static RType Distance(const Vector3 &a, const Vector3 &b)
         {
             return (a - b).Magnitude();
         }
-        static Vector3 ClampMagnitude(const Vector3 &vec, float max_magnitude)
+        static Vector3 ClampMagnitude(const Vector3 &vec, const RType &max_magnitude)
         {
             auto sqr_mag = vec.SqrMagnitude();
             if (sqr_mag > max_magnitude * max_magnitude)
             {
-                auto mag = Vector3Util<RType>::Sqrt(sqr_mag);
+                auto mag = MathUtil::Sqrt(sqr_mag);
                 return Vector3(vec.x_ * max_magnitude / mag, vec.y_ * max_magnitude / mag, vec.z_ * max_magnitude / mag);
             }
             return vec;
@@ -293,7 +306,7 @@ namespace mzx
         }
         static RType Magnitude(const Vector3 &vec)
         {
-            return Vector3Util<RType>::Sqrt(vec.x_ * vec.x_ + vec.y_ * vec.y_ + vec.z_ * vec.z_);
+            return MathUtil::Sqrt(vec.x_ * vec.x_ + vec.y_ * vec.y_ + vec.z_ * vec.z_);
         }
         static Vector3 Min(const Vector3 &lhs, const Vector3 &rhs)
         {
@@ -303,49 +316,57 @@ namespace mzx
         {
             return Vector3(RMax(lhs.x_, rhs.x_), RMax(lhs.y_, rhs.y_), RMax(lhs.z_, rhs.z_));
         }
-        static Vector3 Zero()
+        static const Vector3 &Zero()
         {
-            return Vector3(R_ZERO, R_ZERO, R_ZERO);
+            static const Vector3 zero(R_ZERO, R_ZERO, R_ZERO);
+            return zero;
         }
-        static Vector3 One()
+        static const Vector3 &One()
         {
-            return Vector3(R_ONE, R_ONE, R_ONE);
+            static const Vector3 one(R_ONE, R_ONE, R_ONE);
+            return one;
         }
-        static Vector3 Forward()
+        static const Vector3 &Forward()
         {
-            return Vector3(R_ZERO, R_ZERO, R_ONE);
+            static const Vector3 forward(R_ZERO, R_ZERO, R_ONE);
+            return forward;
         }
-        static Vector3 Back()
+        static const Vector3 &Back()
         {
-            return Vector3(R_ZERO, R_ZERO, -R_ONE);
+            static const Vector3 back(R_ZERO, R_ZERO, -R_ONE);
+            return back;
         }
-        static Vector3 Up()
+        static const Vector3 &Up()
         {
-            return Vector3(R_ZERO, R_ONE, R_ZERO);
+            static const Vector3 up(R_ZERO, R_ONE, R_ZERO);
+            return up;
         }
-        static Vector3 Down()
+        static const Vector3 &Down()
         {
-            return Vector3(R_ZERO, -R_ONE, R_ZERO);
+            static const Vector3 down(R_ZERO, -R_ONE, R_ZERO);
+            return down;
         }
-        static Vector3 Left()
+        static const Vector3 &Left()
         {
-            return Vector3(-R_ONE, R_ZERO, R_ZERO);
+            static const Vector3 left(-R_ONE, R_ZERO, R_ZERO);
+            return left;
         }
-        static Vector3 Right()
+        static const Vector3 &Right()
         {
-            return Vector3(R_ONE, R_ZERO, R_ZERO);
+            static const Vector3 right(R_ONE, R_ZERO, R_ZERO);
+            return right;
         }
 
     private:
-        static RType RMin(RType a, RType b)
+        static RType RMin(const RType &a, const RType &b)
         {
             return a < b ? a : b;
         }
-        static RType RMax(RType a, RType b)
+        static RType RMax(const RType &a, const RType &b)
         {
             return a > b ? a : b;
         }
-        static RType RClamp(RType t, RType mint, RType maxt)
+        static RType RClamp(const RType &t, const RType &mint, const RType &maxt)
         {
             return t < mint ? mint : (t > maxt ? maxt : t);
         }
@@ -355,6 +376,58 @@ namespace mzx
         RType y_;
         RType z_;
     };
+
+    template <>
+    class Vector3MathUtil<float>
+    {
+    public:
+        static float Epsilon()
+        {
+            return 1e-5f;
+        }
+        static float Zero()
+        {
+            return 0;
+        }
+        static float One()
+        {
+            return 1;
+        }
+        static float Two()
+        {
+            return 2;
+        }
+        static float SmoothTimeMin()
+        {
+            return 1e-4f;
+        }
+        static float Dot48()
+        {
+            return 0.48f;
+        }
+        static float DOT235()
+        {
+            return 0.235f;
+        }
+        static float Sqrt(const float &a)
+        {
+            return static_cast<float>(sqrt(a));
+        }
+        static float Rad2Deg(const float &a)
+        {
+            return a * 180 / 3.14159265358979323846;
+        }
+        static float Deg2Rad(const float &a)
+        {
+            return a * 3.14159265358979323846 / 180;
+        }
+        static float Acos(const float &a)
+        {
+            return static_cast<float>(acos(a));
+        }
+    };
+
+    using Vector3f = Vector3<float>;
 } // namespace mzx
 
 #endif
