1d6ce74bc7ecd02c3148cdd1f37bc26c139b8f8c
mzxdream <mengzhaoxiang@xd.com>
2020-12-24 15:07:52 +0800

    1

--- include/mzx/math/fixed_math_util.h
+++ include/mzx/math/fixed_math_util.h
@@ -11,6 +11,8 @@ namespace mzx
     class MathUtil<FixedNumber<T, N, F>>
     {
         using RType = FixedNumber<T, N, F>;
+        using RCType = RType::RType;
+        using RCUType = RType::RUType;
         using RConsts = FixedConsts<T, N>;
 
     public:
@@ -29,53 +31,139 @@ namespace mzx
             static const RType a(RConsts::TWO_PI);
             return a;
         }
-        static constexpr RType Sqrt2()
+        static const RType &Sqrt2()
         {
-            return 1;
+            static const RType a(RConsts::SQRT2);
+            return a;
         }
-        static constexpr RType HalfSqrt2()
+        static const RType &HalfSqrt2()
         {
-            return 1;
+            static const RType a(RConsts::HALF_SQRT2);
+            return a;
         }
-        static constexpr RType TwoSqrt2()
+        static const RType &TwoSqrt2()
         {
-            return 3;
+            static const RType a(RConsts::TWO_SQRT2);
+            return a;
         }
-        static constexpr RType CastFrom(int a)
+        static RType CastFrom(int a)
         {
-            return static_cast<RType>(a);
+            return RType::FromInt(a);
         }
-        static constexpr RType CastFrom(int numerator, int denominator)
+        static RType CastFrom(int numerator, int denominator)
         {
-            return static_cast<RType>(numerator) / static_cast<RType>(denominator);
+            return RType::FromFraction(numerator, denominator);
         }
-        static constexpr int CompareApproximately(RType a, RType b)
+        static int CompareApproximately(const RType &a, const RType &b)
         {
-            return a == b ? 0 : (a < b ? -1 : 1);
+            static const RType t(RConsts::COMPARE_EPSILON);
+            return Abs(a - b) < t ? 0 : (a < b ? -1 : 1);
         }
-        static constexpr RType Abs(RType a)
+        static RType Abs(const RType &a)
         {
-            return std::abs(a);
+            if (a.IsNAN())
+            {
+                return a;
+            }
+            auto mask = a.Get() >> (sizeof(a.Get()) * CHAR_BIT - 1);
+            return RType((a.Get() + mask) ^ mask);
         }
-        static constexpr RType Min(RType a, RType b)
+        static RType Min(const RType &a, const RType &b)
         {
-            return std::min(a, b);
+            return a < b ? a : b;
         }
-        static constexpr RType Max(RType a, RType b)
+        static RType Max(const RType &a, const RType &b)
         {
-            return std::max(a, b);
+            return a < b ? b : a;
         }
-        static constexpr RType Clamp(RType a, RType mina, RType maxa)
+        static RType Clamp(const RType &a, const RType &mina, const RType &maxa)
         {
-            return std::min(std::max(a, mina), maxa);
+            return a < mina ? mina : (a > maxa ? maxa : a);
         }
-        static constexpr RType Lerp(RType a, RType b, RType t)
+        static RType Lerp(const RType &a, const RType &b, const RType &t)
         {
             return a + (b - a) * t;
         }
-        static constexpr RType Sqrt(RType a)
+        static RType Sqrt(const RType &a)
         {
-            return sqrt(a);
+            if (a.IsNAN() || a.IsPosInf())
+            {
+                return a;
+            }
+            if (a < RType::Zero())
+            {
+                return RType::Nan();
+            }
+            if (a == RType::Zero())
+            {
+                return RType::Zero();
+            }
+            RCUType num = a.Get();
+            RCUType res = 0;
+            RCUType bit = static_cast<RCUType>(1) << (sizeof(RCUType) * CHAR_BIT - 2);
+            while (bit > num)
+            {
+                bit >>= 2;
+            }
+            while (bit != 0)
+            {
+                if (num >= res + bit)
+                {
+                    num -= res + bit;
+                    res = (res >> 1) + bit;
+                }
+                else
+                {
+                    res >>= 1;
+                }
+                bit >>= 2;
+            }
+            if (num > RType::R_MASK)
+            {
+                num -= res;
+                num = (num << RType::R_NBITS) - RType::R_HALF;
+                res = (res << RType::R_NBITS) + RType::R_HALF;
+            }
+            else
+            {
+                num <<= RType::R_NBITS;
+                res <<= RType::R_NBITS;
+            }
+            bit = static_cast<RCUType>(1) << (RType::R_NBITS - 2);
+            while (bit != 0)
+            {
+                if (num >= res + bit)
+                {
+                    num -= res + bit;
+                    res = (res >> 1) + bit;
+                }
+                else
+                {
+                    res >>= 1;
+                }
+                bit >>= 2;
+            }
+            if (num > res)
+            {
+                ++res;
+            }
+            return RType(static_cast<RCType>(res));
+        }
+        static RType Pow(const RType &a, int n)
+        {
+            if (n == 0)
+            {
+                return RType::One();
+            }
+            if (n == 1 || a == RType::Zero() || a == RType::One())
+            {
+                return a;
+            }
+            if (n < 0)
+            {
+                return RType::One() / Pow(a, -n);
+            }
+            return (n & 1) == 0 ? Pow(a * a, n >> 1) : Pow(a * a, n >> 1) * a;
         }
         static constexpr RType Cos(RType a)
         {
