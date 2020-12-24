0ac294b7b88216cfb0266eed7005184b7757146a
mzxdream <mengzhaoxiang@xd.com>
2020-12-24 11:47:56 +0800

    1

--- test/math/test_fixed_math_util.h
+++ test/math/test_fixed_math_util.h
@@ -2,8 +2,9 @@
 #include <mzx/math/fixed_math_util.h>
 
 using namespace mzx;
+using Fixed64 = FixedNumber<int64_t, 32>;
 
-void TestFixedMath(int count)
+void TestFixedMathUtil(int count)
 {
     auto c = MathUtil<Fixed64>::CastFrom(10);
     auto b = c;
